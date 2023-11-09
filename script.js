a33a78a72c793d3575c4957b72b5cb7cc82bdc15
Lera <valeriiakolesnikova21@gmail.com>
2023-11-09 20:01:04 +0500

    burger menu added

--- script.js
+++ script.js
@@ -1,2 +1,82 @@
-const app = new Vue();
-console.log(app);
+new Vue({
+    el: '#app',
+    data: {
+        items: [ {
+            name: 'Складская техника',
+            url: 'https://deltainzhiniring.ru/sklad/'
+        },
+        {
+            name: 'Грузоподъёмное оборудование',
+            url: 'https://deltainzhiniring.ru/gruzopodyomnoe/'
+        },
+        {
+            name: 'Станки',
+            url: 'https://deltainzhiniring.ru/stanki/'
+        },
+        {
+            name: 'Строительное оборудование',
+            url: 'https://deltainzhiniring.ru/stroitelnoe/'
+        },
+        {
+            name: 'Окрасочное оборудование',
+            url: 'https://deltainzhiniring.ru/okrasochnoe/'
+        },{
+            name: 'Генераторы и электростанции',
+            url: 'https://deltainzhiniring.ru/generatoryi/'
+        },
+        {
+            name: 'Компрессоры воздушные',
+            url: 'https://deltainzhiniring.ru/kompressoryi'
+        },
+        {
+            name: 'Сварочное оборудование',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },
+        {
+            name: 'Пневматический инструмент',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },
+        {
+            name: 'Профессиональное оборудование для общепита',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },
+        {
+            name: 'Упаковочное и фасовочное оборудование',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },
+        {
+            name: 'Материалы для гидроизоляции деформационных швов',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },        
+        {
+            name: 'Оборудование для обустройства дорог и парковок',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },        
+        {
+            name: 'Гидравлическое оборудование и инструменты',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },        
+        {
+            name: 'Оборудование для автосервиса',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },        
+        {
+            name: 'Оборудование для клининга',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },
+        {
+            name: 'Промышленная вентиляция и отопление',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },
+        {
+            name: 'Ультразвуковое оборудование',
+            url: 'https://deltainzhiniring.ru/sklad'
+        },
+        {
+            name: 'Насосное оборудование',
+            url: 'https://deltainzhiniring.ru/sklad'
+        }
+        ],
+        show:false,
+    },
+})
